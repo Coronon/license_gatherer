d61bdde46eb757f426bc8ef6aee1595154757634
Coronon <33808743+Coronon@users.noreply.github.com>
2021-07-31 00:33:36 +0400

    (Lib) roughly sketch out license compatibility

---
+++ lib/src/license_types.dart
@@ -0,0 +1,16 @@
+class LicenseType {
+  final String name = '';
+  final RegExp regExp = RegExp('');
+  final List<LicenseType> upgradableTo = [];
+}
+
+class TTT implements LicenseType {
+  @override
+  final String name = "t";
+
+  @override
+  final RegExp regExp = RegExp('');
+
+  @override
+  final List<LicenseType> upgradableTo = [LicenseType(), TTT()];
+}
